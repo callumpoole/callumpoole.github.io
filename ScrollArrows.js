a6795ede138543d2b2e496ae91dcb1639af19ad9
callumpoole <14567682+callumpoole@users.noreply.github.com>
2020-02-13 23:12:00 +0700

    Cleaned up js arrows

--- ScrollArrows.js
+++ ScrollArrows.js
@@ -7,54 +7,22 @@ $(window).scroll(function() {
 	
 	//Move arrows 
     if (currentScroll >= h) {
-        $('.fixNavArrows').css({
+        $('#fixedNavArrows').css({
             position: 'fixed',
             top: '0',
             left: '0'
         });
     } else {
-        $('.fixNavArrows').css({
+        $('#fixedNavArrows').css({
             position: 'absolute',
 			top: '100vh' 
         });
     }
 	
 	//Change button functions
-	var csR = Math.round(currentScroll);
-	for (var i = 1; i <= 4; i++){
-		if (Math.abs(i*h - csR) < 2.5){
-			csR = i*h;
-			break;
-		}
-	}
-	if (csR <= h) {
-		$('#arrUp'  ).attr('href',skipIds[0]); 
-	} else if (csR <= 2*h) {
-		$('#arrUp'  ).attr('href',skipIds[1]); 
-	} else if (csR <= 3*h) {
-		$('#arrUp'  ).attr('href',skipIds[2]); 
-	} else if (csR <= 4*h) {
-		$('#arrUp'  ).attr('href',skipIds[3]); 
-	} 
-	if (csR < h) { 
-		$('#arrDown').attr('href',skipIds[1]);
-	} else if (csR < 2*h) { 
-		$('#arrDown').attr('href',skipIds[2]);
-	} else if (csR < 3*h) { 
-		$('#arrDown').attr('href',skipIds[3]);
-	} else if (csR < 4*h) { 
-		$('#arrDown').attr('href',skipIds[4]);
-	}
-	
-	
-	//Dull Text
-	if (currentScroll >= h*1.2) {
-        $('.sideBan1').css({ 
-            opacity: '0.3'
-        });
-    } else {
-        $('.sideBan1').css({ 
-			opacity: '1'
-        });
-    }
+	var scrollRounded = Math.round(currentScroll);
+	const closeTo = 2.5;
+
+	$('#arrUp'  ).attr('href',skipIds[Math.floor((scrollRounded - closeTo) / h)]); 
+	$('#arrDown').attr('href',skipIds[Math.ceil ((scrollRounded + closeTo) / h)]); 
 });
